08e47ae87b24c928d1ce9815e464bc19bc9f2bae
EuhenyP <tennecotecnic@gmail.com>
2023-03-06 22:59:28 +0300

    Initial commit.

---
+++ src/main/java/com/prozorov/javaexperiments/array/ArrayMain.java
@@ -0,0 +1,7 @@
+package com.prozorov.javaexperiments.array;
+
+public class ArrayMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/prozorov/javaexperiments/collection/CollectionMain.java
@@ -0,0 +1,7 @@
+package com.prozorov.javaexperiments.collection;
+
+public class CollectionMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
