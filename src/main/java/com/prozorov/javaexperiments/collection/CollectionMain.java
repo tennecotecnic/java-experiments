64a6334e82608097c8c5c2ea37ab930cd8ee3ae1
EuhenyP <tennecotecnic@gmail.com>
2023-03-06 23:14:24 +0300

    String list added.

--- src/main/java/com/prozorov/javaexperiments/collection/CollectionMain.java
+++ src/main/java/com/prozorov/javaexperiments/collection/CollectionMain.java
@@ -1,6 +1,9 @@
 package com.prozorov.javaexperiments.collection;
 
+import java.util.List;
+
 public class CollectionMain {
+    private List<String> list;
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
