2911af0b9057daba03d786c7131f6b851270b2e0
EuhenyP <tennecotecnic@gmail.com>
2023-03-06 23:05:20 +0300

    String array added.

--- src/main/java/com/prozorov/javaexperiments/array/ArrayMain.java
+++ src/main/java/com/prozorov/javaexperiments/array/ArrayMain.java
@@ -1,6 +1,8 @@
 package com.prozorov.javaexperiments.array;
 
 public class ArrayMain {
+
+    private static String[] stringArray;
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
