0eefaddba68366b7ce779d132740afa97bea4fcb
EuhenyP <tennecotecnic@gmail.com>
2023-03-06 23:02:54 +0300

    String functional added.

--- src/main/java/com/prozorov/javaexperiments/string/StringMain.java
+++ src/main/java/com/prozorov/javaexperiments/string/StringMain.java
@@ -3,5 +3,7 @@ package com.prozorov.javaexperiments.string;
 public class StringMain {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println(String.format("Hello, %s", "Euheny"));
     }
 }
