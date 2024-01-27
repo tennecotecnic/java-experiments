7eaa84f4a7148f286af5c6375cdde0881212e89f
EuhenyP <tennecotecnic@gmail.com>
2024-01-27 22:26:11 +0300

    develop 2

--- src/main/java/com/prozorov/javaexperiments/threads/Threads.java
+++ src/main/java/com/prozorov/javaexperiments/threads/Threads.java
@@ -8,6 +8,8 @@ public class Threads {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
 
     }
     }
