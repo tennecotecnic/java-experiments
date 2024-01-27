f4b66738097bae0969b4f0d8cc5c5338e1906bb1
EuhenyP <tennecotecnic@gmail.com>
2024-01-27 22:25:41 +0300

    develop 1

--- src/main/java/com/prozorov/javaexperiments/threads/Threads.java
+++ src/main/java/com/prozorov/javaexperiments/threads/Threads.java
@@ -2,6 +2,7 @@ package com.prozorov.javaexperiments.threads;
 
 public class Threads {
     int i = 5;
+    String s;
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
