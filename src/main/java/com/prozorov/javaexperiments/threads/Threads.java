dc1a6a5e88409ec179950642485f6adc38a898ed
EuhenyP <tennecotecnic@gmail.com>
2024-01-27 21:54:14 +0300

    testbranch1 test 1.

--- src/main/java/com/prozorov/javaexperiments/threads/Threads.java
+++ src/main/java/com/prozorov/javaexperiments/threads/Threads.java
@@ -6,6 +6,8 @@ public class Threads {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("testbranch1");
+
 
     }
     }
