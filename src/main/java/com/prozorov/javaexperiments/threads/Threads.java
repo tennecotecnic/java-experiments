8b6389f3b1b6249d94c26473590a3f8287a92f63
EuhenyP <tennecotecnic@gmail.com>
2024-01-27 21:49:52 +0300

    Master test 2.

--- src/main/java/com/prozorov/javaexperiments/threads/Threads.java
+++ src/main/java/com/prozorov/javaexperiments/threads/Threads.java
@@ -4,6 +4,9 @@ public class Threads {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
 
     }
     }
