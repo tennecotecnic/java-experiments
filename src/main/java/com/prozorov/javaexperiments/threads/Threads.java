1207c23c9adffa9a97c48db9c745b926d663c7ed
EuhenyP <tennecotecnic@gmail.com>
2024-01-27 22:27:18 +0300

    master 5

--- src/main/java/com/prozorov/javaexperiments/threads/Threads.java
+++ src/main/java/com/prozorov/javaexperiments/threads/Threads.java
@@ -7,6 +7,7 @@ public class Threads {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("Hello ");
 
     }
     }
